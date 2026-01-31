c4947055a97c8d5c5d7583e51a8007a113658b9e
Алтынай Ертай <altynayyertay@MacBook-Air-Altynaj.local>
2026-01-31 22:40:18 +0500

    feat(altynay): setup HTTP server and environment config

--- main.go
+++ main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"cinema/internal/api"
+	"cinema/internal/models"
+	"cinema/internal/service"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("CinemaGo Server is running...")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system variables")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/movies", getMovieHandler)
+	http.HandleFunc("/book", createBookingHandler)
+	http.HandleFunc("/orders", listOrdersHandler)
+
+	fmt.Printf("CinemaGo Server started at http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func getMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		id = "157336"
+	}
+
+	movie, err := api.FetchMovieDetails(id)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movie)
+}
+
+func createBookingHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Use POST", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var input struct {
+		Email     string `json:"email"`
+		MovieID   string `json:"movie_id"`
+		IsStudent bool   `json:"is_student"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	movie, err := api.FetchMovieDetails(input.MovieID)
+	movieTitle := "Unknown Movie"
+	if err == nil {
+		movieTitle = movie.Title
+	}
+
+	finalPrice := service.CalculatePrice(2000.0, input.IsStudent)
+
+	newOrder := models.Order{
+		CustomerEmail: input.Email,
+		MovieTitle:    movieTitle,
+		FinalPrice:    finalPrice,
+	}
+
+	models.SaveOrder(newOrder)
+
+	service.SendAsyncNotification(input.Email, movieTitle)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"status": "Success",
+		"order":  newOrder,
+		"note":   "Check console for background goroutine output",
+	})
+}
+
+func listOrdersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(models.GetAllOrders())
 }
