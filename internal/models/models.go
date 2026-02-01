e9826b7e2c0d2736d0f9c8bbf37d8dd40a44a17c
Aknur <aknur_salemxan@mail.ru>
2026-02-01 13:10:17 +0500

    fix(aknur): keep models.go with types only

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,11 +1,17 @@
 package models
 
-import "sync"
-
 type Movie struct {
 	ID       int    `json:"id"`
 	Title    string `json:"title"`
 	Overview string `json:"overview"`
+}
+
+type Session struct {
+	ID             int      `json:"id"`
+	MovieID        int      `json:"movie_id"`
+	MovieTitle     string   `json:"movie_title"`
+	BasePrice      float64  `json:"base_price"`
+	AvailableSeats []string `json:"available_seats"`
 }
 
 type Order struct {
@@ -13,22 +19,4 @@ type Order struct {
 	CustomerEmail string  `json:"customer_email"`
 	MovieTitle    string  `json:"movie_title"`
 	FinalPrice    float64 `json:"final_price"`
-}
-
-var (
-	orders      []Order
-	ordersMutex sync.Mutex
-)
-
-func SaveOrder(o Order) {
-	ordersMutex.Lock()
-	defer ordersMutex.Unlock()
-	o.ID = len(orders) + 1
-	orders = append(orders, o)
-}
-
-func GetAllOrders() []Order {
-	ordersMutex.Lock()
-	defer ordersMutex.Unlock()
-	return orders
 }
